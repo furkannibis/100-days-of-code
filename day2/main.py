7db1fc92da8ffb57d8d61b03fa0f4c86dbf71f5e
Furkan İBİŞ <ibis.furkann@gmail.com>
2024-08-04 21:22:22 +0300

    Day-2 TIP Calculator

---
+++ day2/main.py
@@ -0,0 +1,29 @@
+print('Hello'[0])
+print('Hello'[1])
+print('123' + '456')
+
+print(123 + 456)
+print(123_456 + 789_012)
+
+print(3.14159)
+
+print(True)
+print(False)
+
+num_char = len(input('What is your name?\n'))
+print(type(num_char))
+print('Your name has ' + str(num_char) + ' characters.')
+
+print(6 / 3)
+print(type(6 / 3))
+print(2 ** 4)
+print(3 * 3 + 3 / 3 - 3)
+
+print(8 / 3)
+print(round(8 / 3))
+print(round(8 / 3, 2))
+print(8 // 3)
+print(type(8 // 3))
+
+score = 100
+print(f'Your score is {score}.')
