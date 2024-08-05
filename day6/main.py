d3fea61239709d4efbb7874d71eb400c697c8833
Furkan İBİŞ <ibis.furkann@gmail.com>
2024-08-06 00:33:10 +0300

    Day-6 Escaping the Maze

---
+++ day6/main.py
@@ -0,0 +1,32 @@
+print('Hello')
+num_char = len('Hello')
+print(num_char)
+
+def my_func():
+    print('Hello')
+    print('Bye')
+
+my_func()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def turn_back():
+#     turn_left()
+#     turn_left()
+    
+
+# while True:
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
+#     if wall_in_front():
+#         turn_left()
+#     elif at_goal():
+#         break
+#     else:
+#         move()
